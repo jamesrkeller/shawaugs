6b625b6ce9449f15e63633aa9eec639a6bb4a865
James Keller <james.keller@sjrb.ca>
2011-08-10 16:27:06 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -2,9 +2,15 @@
 {
     public class FrontController : IProcessWebRequests
     {
+    	IFindCommandsThatCanProcessRequests registry;
+		public FrontController(IFindCommandsThatCanProcessRequests registry)
+		{
+			this.registry = registry;
+		}
+
         public void process(IContainRequestInformation the_request)
         {
-            throw new System.NotImplementedException();
+        	registry.get_command_for(the_request).process(the_request);
         }
     }
 }
