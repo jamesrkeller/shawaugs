6bbcc950c3528a93a0031d3b9f8e9f95293e6782
James Keller <james.keller@sjrb.ca>
2011-08-11 09:42:51 -0600

    Pushing new changes

--- source/nothinbutdotnetstore.specs/ViewMainDepartmentsInTheStoreSpecs.cs
+++ source/nothinbutdotnetstore.specs/ViewMainDepartmentsInTheStoreSpecs.cs
@@ -1,6 +1,9 @@
-﻿ using Machine.Specifications;
+﻿ using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using Machine.Specifications;
  using developwithpassion.specifications.rhinomocks;
- using developwithpassion.specifications.extensions;
+ using nothinbutdotnetstore.web.application.catalogbrowsing;
  using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.specs
@@ -8,18 +11,30 @@ namespace nothinbutdotnetstore.specs
     [Subject(typeof(ViewMainDepartmentsInTheStore))]  
     public class ViewMainDepartmentsInTheStoreSpecs
     {
-        public abstract class concern : Observes<IProcessAnApplicationBehaviour,
-                                            ViewMainDepartmentsInTheStore>
+        public abstract class concern : Observes<IProcessAnApplicationBehaviour, ViewMainDepartmentsInTheStore>
         {
-        
+			
         }
 
    
         public class when_processing_a_request : concern
         {
-        
-            It first_observation = () =>        
-                
+        	Establish c = () =>
+        	{
+				depends.on<Func<IEnumerable<Department>>>(() => Enumerable.Range(1, 10).Select(x => new Department()));
+        		depends.on<Action<IEnumerable<Department>>>(departments => view_data = departments);
+
+        		request = fake.an<IContainRequestInformation>();
+        	};
+
+        	Because b = () =>
+        		sut.process(request);
+
+        	It should_provide_departments_to_view = () =>
+        		view_data.ShouldNotBeEmpty();
+
+        	static IContainRequestInformation request;
+        	static IEnumerable<Department> view_data;
         }
     }
 }
---
+++ source/nothinbutdotnetstore/web/application/catalogbrowsing/ViewMainDepartmentsInTheStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.web.application.catalogbrowsing
+{
+	public class ViewMainDepartmentsInTheStore : IProcessAnApplicationBehaviour
+	{
+		public void process(IContainRequestInformation request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class Department
+	{
+		
+	}
+}
