83cbbe47825f2fdfe36ae2d890e2146f5070dcc2
James Keller <james.keller@sjrb.ca>
2011-08-11 08:47:48 -0600

    Pushing new changes

---
+++ source/nothinbutdotnetstore.specs/RequestCommandSpecs.cs
@@ -0,0 +1,35 @@
+﻿ using System;
+ using Machine.Specifications;
+ using developwithpassion.specifications.rhinomocks;
+ using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs
+{  
+    [Subject(typeof(RequestCommand))]  
+    public class RequestCommandSpecs
+    {
+        public abstract class concern : Observes<IProcessOneSpecificRequest, RequestCommand>
+        {
+        
+        }
+
+   
+        public class when_determining_whether_it_can_handle_a_request : concern
+        {
+            Establish c = () =>
+            {
+            	depends.on<Predicate<IContainRequestInformation>>(x => true);
+            	request = fake.an<IContainRequestInformation>();
+            };
+
+        	Because b = () =>
+        		result = sut.can_process(request);
+			
+        	It should_delegate_the_processing_to_the_command_that_can_process_the_request = () =>
+        		result.ShouldBeTrue();
+
+            static IContainRequestInformation request;
+        	static bool result;
+        }
+    }
+}
--- source/nothinbutdotnetstore/web/core/IProcessAWebRequest.cs
+++ source/nothinbutdotnetstore/web/core/IProcessAWebRequest.cs
@@ -1,8 +1,29 @@
-﻿namespace nothinbutdotnetstore.web.core
+﻿using System;
+
+namespace nothinbutdotnetstore.web.core
 {
     public interface IProcessOneSpecificRequest 
     {
         void process(IContainRequestInformation the_request);
         bool can_process(IContainRequestInformation request);
     }
+
+	public class RequestCommand : IProcessOneSpecificRequest
+	{
+		Predicate<IContainRequestInformation> can_handle;
+		public RequestCommand(Predicate<IContainRequestInformation> can_handle)
+		{
+			this.can_handle = can_handle;
+		}
+
+		public bool can_process(IContainRequestInformation request)
+		{
+			return can_handle(request);
+		}
+
+		public void process(IContainRequestInformation the_request)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
